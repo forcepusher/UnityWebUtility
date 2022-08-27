238c416445048ceceac58279cf79951cf90516c7
forcepusher <bionitsoup@gmail.com>
2022-08-27 22:17:58 +0700

    Add audio mute to playtest

--- Packages/com.agava.webutility/Samples/Playtesting/PlaytestingCanvas.cs
+++ Packages/com.agava.webutility/Samples/Playtesting/PlaytestingCanvas.cs
@@ -4,6 +4,13 @@ namespace Agava.WebUtility.Samples
 {
     public class PlaytestingCanvas : MonoBehaviour
     {
+        private void Update()
+        {
+#if !UNITY_WEBGL || UNITY_EDITOR
+            return;
+#endif
 
+            AudioListener.pause = WebApplication.InBackground;
+        }
     }
 }
