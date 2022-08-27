0698408fb5f9a6e20d8a5ffe61739e200d702014
forcepusher <bionitsoup@gmail.com>
2022-08-28 01:00:17 +0700

    Work on visibility change event

--- Packages/com.agava.webutility/Runtime/WebApplication.cs
+++ Packages/com.agava.webutility/Runtime/WebApplication.cs
@@ -1,12 +1,38 @@
+using AOT;
+using System;
 using System.Runtime.InteropServices;
 
 namespace Agava.WebUtility
 {
     public static class WebApplication
     {
+        /// <remarks>
+        /// Triggers way faster than InBackground, but
+        /// I will break your legs if you forget to unsubscribe from this event.
+        /// </remarks>
+        public static event Action<bool> InBackgroundChange;
+
         public static bool InBackground => GetWebApplicationInBackground();
 
         [DllImport("__Internal")]
         private static extern bool GetWebApplicationInBackground();
+
+        [DllImport("__Internal")]
+        private static extern bool SetWebApplicationInBackgroundChangeCallback(Action<bool> callback);
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+#endif
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Unity InitializeOnLoadMethod")]
+        private static void Initialize()
+        {
+            SetWebApplicationInBackgroundChangeCallback(OnInBackgroundChange);
+        }
+
+        [MonoPInvokeCallback(typeof(Action<bool>))]
+        private static void OnInBackgroundChange(bool hidden)
+        {
+            InBackgroundChange?.Invoke(hidden);
+        }
     }
 }
--- Packages/com.agava.webutility/Runtime/WebApplication.jslib
+++ Packages/com.agava.webutility/Runtime/WebApplication.jslib
@@ -1,14 +1,27 @@
 const library = {
-
     // Class definition.
   
+    inBackgroundChangeCallbackPtr: undefined,
+
     $webApplication: {
+        setInBackgroundChangeCallback: function (callbackPtr) {
+            webApplication.inBackgroundChangeCallbackPtr = callbackPtr;
+
+            document.addEventListener('visibilitychange', function () {
+                dynCall('vii', callbackPtr, [errorUnmanagedStringPtr]);
+            });
+        },
+
         getInBackground: function () {
             return document.hidden;
-          },
+        },
     },
 
     // External C# calls.
+
+    SetWebApplicationInBackgroundChangeCallback: function (callbackPtr) {
+        webApplication.setInBackgroundChangeCallback(callbackPtr);
+    },
 
     GetWebApplicationInBackground: function () {
         return webApplication.getInBackground();
