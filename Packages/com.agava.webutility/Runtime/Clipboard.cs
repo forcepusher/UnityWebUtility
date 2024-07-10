0b9ab79892f66a6784f63153da4a5e429e4e29f8
forcepusher <bionitsoup@gmail.com>
2024-07-10 20:54:09 +0700

    Add auto clipboard initialization

--- Packages/com.agava.webutility/Runtime/Clipboard.cs
+++ Packages/com.agava.webutility/Runtime/Clipboard.cs
@@ -1,7 +1,25 @@
+using System.Runtime.InteropServices;
+#if UNITY_WEBGL && !UNITY_EDITOR
+using UnityEngine;
+using UnityEngine.Scripting;
+
+[assembly: AlwaysLinkAssembly]
+#endif
+
 namespace Agava.WebUtility
 {
     public static class Clipboard
     {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+#endif
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Unity InitializeOnLoadMethod")]
+        private static void Initialize()
+        {
+            ClipboardInitialize();
+        }
 
+        [DllImport("__Internal")]
+        private static extern bool ClipboardInitialize();
     }
 }
